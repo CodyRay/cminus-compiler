70053b843dc80c667a631feb7614bb610bec795a
Cody Ray Freeman Hoeft <crhoeft@gmail.com>
2017-03-17 02:12:52 -0700

    Add App code, program files

--- src/main/java/io/github/haroldhues/App.java
+++ src/main/java/io/github/haroldhues/App.java
@@ -1,16 +1,72 @@
 package io.github.haroldhues;
 
-/**
- * Hello world!
- *
- */
+import java.io.*;
+
+import io.github.haroldhues.SyntaxTree.*;
+import io.github.haroldhues.Tokens.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        for(String arg: args) {
-            System.out.println(arg);
+        String fileName = args[0];
+        try {
+            if (!fileName.matches(".*\\.cm")) {
+                throw new CompileErrorException("The provided file (" + fileName + ") name does not appear to be a *.cm file, please rename it");
+            }
+            Enumerable<Character> file = new FileScanner(fileName);
+            Enumerable<Token> tokens = new Scanner(file);
+            Parser parser = new Parser(tokens);
+            RootNode node = parser.parse();
+            Checker.check(node);
+            Interpreter.run(node, new IO());
+        } catch (FileNotFoundException ex) {
+            System.out.println("The file (" + fileName + ") doesn't seem to exist!: " + ex.getMessage());
+        } catch (CompileErrorException ex) {
+            System.out.println("Compile Error: " + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Unexpected Error: " + ex.getMessage());
+        }
+    }
+
+    public static class FileScanner extends Enumerable<Character> {
+        FileReader reader = null;
+
+        public FileScanner(String fileName) throws FileNotFoundException {
+            reader = new FileReader(fileName);
+        }
+
+        public Character next() {
+            try {
+                int c = reader.read();
+                if(c != -1) {
+                    return (char) c;
+                } else {
+                    return null;
+                }
+            } catch (IOException ex) {
+                return null;
+            }
+        }
+    }
+
+    public static class IO implements InputOutput {
+        public int read() {
+            System.out.print("> ");
+            String text = System.console().readLine();
+            Integer in = null;
+            while(in == null) {
+                try {
+                    in = Integer.parseInt(text);
+                } catch (NumberFormatException ex) {
+                    System.out.println("Try Again...");
+                }
+            }
+            return in;
+        }
+
+        public void write(int out) {
+            System.out.println(out);
         }
     }
 }
